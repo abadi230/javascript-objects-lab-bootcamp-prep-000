3b065a70b2097b06199a2171a4b7a0da227f6b1a
Abdullah Bajaman <ambajaman@gmail.com>
2019-06-25 12:33:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,11 +29,11 @@ function destructivelyDeleteFromObjectByKey(object, key){
   */
   var recipes = {}
   
-  // function updateObjectWithKeyAndValue(object, key, value){
-    
-  //   object[key] = value;
-  //   return object;
-  // }
   function updateObjectWithKeyAndValue(object, key, value){
-    return Object.assign({}, Object,{ [key]: value})
+    
+    object[key] = value;
+    return object;
   }
+  // function updateObjectWithKeyAndValue(object, key, value){
+  //   return Object.assign({}, Object,{ [key]: value})
+  // }
